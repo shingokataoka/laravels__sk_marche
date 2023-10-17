f3b91a3def20cfe3745d57c7cc8c72732cf82662
shingokataoka <shingo.kataoka777@gmail.com>
2023-10-18 00:04:47 +0900

    laravel reactの環境設定

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Inertia\Inertia;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,5 +22,30 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+
+        // reactで__('Dashboard')など言語メソッドを使えるようにする
+        $this->reactFuncTransCreate();
+    }
+
+    // reactで__('Dashboard')など言語メソッドを使えるようにする
+    // 翻訳データを Inertia へ渡して、
+    // jsx側では{ __('Dashboard') } などで言語ファイルの、設定文字列を表示する
+    private function reactFuncTransCreate() {
+        $locale_data = [];
+        // $locales = ['ja']; // 👈 できれば Enum での管理がベターです
+        $json_paths = glob(lang_path(). '/*.json');
+
+        foreach ($json_paths as $json_path) {
+            $locale = pathinfo($json_path)['filename'];
+            $json_content = file_get_contents($json_path);
+            $locale_data[$locale] = json_decode($json_content, true);
+        }
+        $default_locale = config('app.locale');
+        $current_locale = request('locale', $default_locale);
+
+        Inertia::share('locale', [
+            'currentLocale' => $current_locale,
+            'localeData' => $locale_data,
+        ]);
     }
 }
